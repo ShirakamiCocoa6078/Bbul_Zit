32ebc03484580a4f71552f7c33d236889c1d0fd2
Hotococoa6078 <71052778+Hotococoa6078@users.noreply.github.com>
2022-12-05 11:36:45 +0900

    Update and rename asdf to asdf.py

---
+++ asdf.py
@@ -0,0 +1,14 @@
+import random
+"5weapon 80 chunjyang, 66~ %up, 2 pickddul 1pickup, 4 10stack"
+"5character 90 chunhyang, 76~ %up, 1 pickddul 1 pickup, 4 10stack"
+#확률은 0.600부터 시작, %up부터 76 32%~ 89까지 33%, 1회당 0.08333333333333333씩 퍼센트업
+stack = 0
+def clearGet():
+    get = {'3weapon' = 0, '4pickCharacter' = 0, '4pickWeapon' = 0, '4character' = 0, 
+    '4weapon' = 0, '5pickCharacter' = 0, '5pickWeapon' = 0, '5character' = 0, '5weapon' = 0}
+def PickChar10():
+    for i in range(1, 10):
+        
+value = input('kind of gacha(weapon/character) : ')
+if value = "character":
+    
