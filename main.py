96e92ae3608e33b16b460d1677d867af0deded11
Hotococoa6078 <71052778+Hotococoa6078@users.noreply.github.com>
2023-01-27 04:20:25 +0000

    very_important_update

--- main.py
+++ main.py
@@ -4,19 +4,28 @@ _rate5 = .006   # 0.6%
 _rate4 = .051   # 5.1%
 _pity5 = 73	    # 5* pity starts after 73 attempts
 _pity4 = 8      # 4* pity starts after 8 attempts
-# counter5 and counter4 are the (persistent) user state for this banner (initialize to 1)
-x = random.randint(10) * 0.1    # between 0.0 and 1.0
-prob5 = _rate5 + max(0, (counter5-_pity5) * 10 * _rate5)
-prob4 = _rate4 + max(0, (counter4-_pity4) * 10 * _rate4)
+stack = 0
+stack4 = [0, False] # stack of 4 star
+stack5 = [0, False] # stack of 5 star
+get = {'3W' : 0, '4PC' : 0, '4PW' : 0, '4C' : 0, '4W' : 0, '5PC' : 0, '5PW' : 0, '5C' : 0, '5W' : 0}
+def clearGet():
+    global get
+    get = {'3W' : 0, '4PC' : 0, '4PW' : 0, '4C' : 0, '4W' : 0, '5PC' : 0, '5PW' : 0, '5C' : 0, '5W' : 0}
+prob5, prob4, x = 0,0,0
+def setPer():
+    global x, prob5, prob4
+    x = random.randint(1, 10) * 0.01 # between 0.0 and 1.0
+    prob5 = _rate5 + max(0, (stack5[0]-_pity5) * 10 * _rate5)
+    prob4 = _rate4 + max(0, (stack4[0]-_pity4) * 10 * _rate4)
 if (x < prob5):
 	#drop(5star)
-	counter5 = 1;
+	counter5 = 1
 	counter4 += 1
 else if (x < prob4 + prob5):
 	#drop(4star)
-	counter5 += 1;
-	counter4 = 1;
+	counter5 += 1
+	counter4 = 1
 else:
 	#drop(3star)
-	counter5 += 1;
-	counter4 += 1;
+	counter5 += 1
+	counter4 += 1
